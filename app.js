e09bb141ec4eafaab209f9962167a770c6ab41dc
mohammadbader99 <>
2021-07-25 12:29:50 +0000

    Used functions

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ while(catsNum < 1 || catsNum > 10){
 if(favCat == "British"){
   for(var i = 1; i <= catsNum; i++){
     document.write(
-      "<h4>" + favCat + " #" + i + "</h4>" + "<img src='http://www.pngplay.com/wp-content/uploads/6/British-Shorthair-Cat-PNG-Photos.png' alt='British Shorthair' width='230' height='230' title='British Shorthair'>"
+      addBritish()
     )
   }
 }
@@ -39,7 +39,7 @@ if(favCat == "British"){
 else if(favCat == "Persian"){
   for(var i = 1; i <= catsNum; i++){
     document.write(
-      "<h4>" + favCat + " #" + i + "</h4>" + "<img src='Pictures/Persian.png' alt='Persian' width='150' height='213' title='Persian'>"
+      addPersian()
     )
   }
 }
@@ -47,7 +47,19 @@ else if(favCat == "Persian"){
 else if(favCat == "Teacup"){
   for(var i = 1; i <= catsNum; i++){
     document.write(
-      "<h4>" + favCat + " #" + i + "</h4>" + "<img src='Pictures/Teacup.png' alt='Teacup' width='230' height='230' title='Teacup'>"
+      addTeacup()
     )
   }
+}
+
+function addBritish(){
+  return "<h4>" + favCat + " #" + i + "</h4>" + "<img src='http://www.pngplay.com/wp-content/uploads/6/British-Shorthair-Cat-PNG-Photos.png' alt='British Shorthair' width='230' height='230' title='British Shorthair'>"
+}
+
+function addPersian(){
+  return "<h4>" + favCat + " #" + i + "</h4>" + "<img src='Pictures/Persian.png' alt='Persian' width='150' height='213' title='Persian'>"
+}
+
+function addTeacup(){
+  return "<h4>" + favCat + " #" + i + "</h4>" + "<img src='Pictures/Teacup.png' alt='Teacup' width='230' height='230' title='Teacup'>"
 }
